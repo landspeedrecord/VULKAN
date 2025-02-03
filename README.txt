0990b66211c7bae36dd3649fb2c891dc619aa879
landspeedrecord <nmoagb@gmail.com>
2025-02-03 03:38:59 +0100

    first commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+how to compile with developer command prompt for VS 2022:
+0. have visual studio installed with the desktop development option
+1. open developer command prompt for VS 2022
+2. navigate to the folder where "main.c" is
+3. use command: cl main.c user32.lib gdi32.lib
---
+++ main.c
@@ -0,0 +1,72 @@
+#include <windows.h>
+
+// Window procedure to handle messages
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+        case WM_PAINT: {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+
+            // Fill the client area with a blue color
+            RECT rect;
+            GetClientRect(hwnd, &rect);
+            HBRUSH hBrush = CreateSolidBrush(RGB(0, 0, 255));
+            FillRect(hdc, &rect, hBrush);
+            DeleteObject(hBrush);
+
+            EndPaint(hwnd, &ps);
+            break;
+        }
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
+}
+
+// Entry point
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    
+    const char CLASS_NAME[] = "BlueWindowClass";
+
+    WNDCLASS wc = {0};
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    // Create the window
+    HWND hwnd = CreateWindowEx(
+        0,                              // Optional window styles
+        CLASS_NAME,                     // Window class
+        "Blue Background Window",       // Window title
+        WS_OVERLAPPEDWINDOW,            // Window style
+
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL,       // Parent window
+        NULL,       // Menu
+        hInstance,  // Instance handle
+        NULL        // Additional application data
+    );
+
+    if (hwnd == NULL) {
+        return 0;
+    }
+
+    // Show the window
+    ShowWindow(hwnd, nCmdShow);
+
+    // Message loop
+    MSG msg = {0};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
